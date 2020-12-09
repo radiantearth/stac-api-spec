888fea9d383953767b1f7fefeda316f2c2a8d3be
Matthew Hanson <matt.a.hanson@gmail.com>
2020-12-08 23:28:44 -0500

    ci: update openapi build

--- .circleci/build-openapi.sh
+++ .circleci/build-openapi.sh
@@ -9,5 +9,23 @@ FNAMES=`find . -name "openapi.yaml" -not -path "./fragments/*" -not -path "./nod
 for fin in $FNAMES; do
     fout=./build/$fin
     mkdir -p ${fout%/*}
-    speccy resolve $fin > $fout
+    #speccy resolve $fin > $fout
+    swagger-cli bundle $fin -o $fout -t yaml
+    cp build/index.html ${fout%/*}/
 done
+
+# use swagger-combine
+#swagger-combine swagger.yaml -o build/STAC-complete.yml
+
+# use openapi-merge-cli
+#openapi-merge-cli -c openapi-merge-config.json
+#json2yaml build/STAC-complete.json > build/STAC-complete.yml
+
+# use yq
+yq merge \
+    build/core/openapi.yaml \
+    build/item-search/openapi.yaml \
+    build/ogcapi-features/openapi.yaml \
+    build/ogcapi-features/extensions/transaction/openapi.yaml \
+    build/ogcapi-features/extensions/version/openapi.yaml \
+    | tee build/openapi.yaml
