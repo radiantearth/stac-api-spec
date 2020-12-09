c7994a38172a05a268c243f04154f2d18a6a3a51
Matthew Hanson <matt.a.hanson@gmail.com>
2020-12-09 01:35:24 -0500

    ci: add swagger-config file

--- .circleci/build-openapi.sh
+++ .circleci/build-openapi.sh
@@ -10,22 +10,22 @@ for fin in $FNAMES; do
     fout=./build/$fin
     mkdir -p ${fout%/*}
     #speccy resolve $fin > $fout
-    swagger-cli bundle $fin -o $fout -t yaml
+    swagger-cli bundle $fin -o $fout -t yaml -r
     cp build/index.html ${fout%/*}/
 done
 
 # use swagger-combine
-#swagger-combine swagger.yaml -o build/STAC-complete.yml
+swagger-combine build/swagger.yaml -o build/openapi.yaml
 
 # use openapi-merge-cli
-#openapi-merge-cli -c openapi-merge-config.json
-#json2yaml build/STAC-complete.json > build/STAC-complete.yml
+#openapi-merge-cli -c build/openapi-merge-config.json
+#json2yaml build/openapi-merge.json > build/openapi-merge.yaml
 
 # use yq
-yq merge -a \
-    build/core/openapi.yaml \
-    build/item-search/openapi.yaml \
-    build/ogcapi-features/openapi.yaml \
-    build/ogcapi-features/extensions/transaction/openapi.yaml \
-    build/ogcapi-features/extensions/version/openapi.yaml \
-    | tee build/openapi.yaml
+#yq merge -a \
+#    build/core/openapi.yaml \
+#    build/item-search/openapi.yaml \
+#    build/ogcapi-features/openapi.yaml \
+#    build/ogcapi-features/extensions/transaction/openapi.yaml \
+#    build/ogcapi-features/extensions/version/openapi.yaml \
+#    | tee build/openapi-yq.yaml
